1d6b4836223180a27e2b227cc2fb7151d76dbc7c
acegrin <acegrin@gmail.com>
2026-05-11 14:01:52 +0200

    Update

--- payments/firebase_client.py
+++ payments/firebase_client.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore, auth
 
 # Only initialize once (avoid re-init when Django reloads)
 if not firebase_admin._apps:
-    cred = credentials.Certificate("/home/parafinah/parafinah_backend/firebase_key.json")
+    cred = credentials.Certificate("/home/parafinah/firebase_key.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
